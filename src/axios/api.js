4b2b928c18f36fd21f904fd3cd0a1592f862096e
zhulingqingtian <1399873403@qq.com>
2019-02-17 15:37:54 +0800

    Mod: 新增xx接口

--- src/axios/api.js
+++ src/axios/api.js
@@ -38,7 +38,7 @@ axios.interceptors.request.use(function(config) {
     return Promise.reject(error);
   });
 
-// 响应拦截器
+// 响应拦截器aaa
 axios.interceptors.response.use(function(response) {
   return response;
 }, function(error) {
